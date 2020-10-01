033c27f93e60e1dbf8b9c5530f2f58b9323c9252
Valentin Sanchez <elzero_2@hotmail.com>
2020-10-01 13:23:14 +0200

    Initial Commit

---
+++ FirstEscapeApp/FirstEscapeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstEscapeAppApp.swift
+//  FirstEscapeApp
+//
+//  Created by Valentín Sánchez Campañón on 01/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct FirstEscapeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
